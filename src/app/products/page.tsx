dff3ce31c336a48fc3953175709e9ff67465537d
Yoga S Devanada <ydevanada@gmail.com>
2024-05-15 07:52:57 +0700

    initial commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div>
+      Page Products
+      <Button variant="outline">Click me!</Button>
+    </div>
+  );
+}
